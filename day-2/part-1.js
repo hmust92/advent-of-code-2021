408648d9df0c8bd23366b8f365cf2389cd5b5f90
Hasan <hmust92@gmail.com>
2021-12-19 06:10:31 -0600

    removes confusing type variable

--- day-2/part-1.js
+++ day-2/part-1.js
@@ -10,15 +10,14 @@ const { [HORIZONTAL]: horizontal, [DEPTH]: depth } = input.reduce(
     (accumulator, data, index, originalList) => {
         const direction = data.split(" ")[0];
         const value = Number(data.split(" ")[1]);
-        const type = direction === FORWARD ? HORIZONTAL : DEPTH;
 
         const result = { ...accumulator };
 
-        if (type === HORIZONTAL) {
-            result[type] = result[type] + value;
+        if (direction === FORWARD) {
+            result[HORIZONTAL] = result[HORIZONTAL] + value;
         } else {
-            result[type] =
-                direction === UP ? result[type] - value : result[type] + value;
+            result[DEPTH] =
+                direction === UP ? result[DEPTH] - value : result[DEPTH] + value;
         }
 
         return result;
